8bc743b3250a19ed9cfe2618231a1b397834bc9d
Roy Park <roypark2638@gmail.com>
2021-04-27 23:16:30 -0700

    getting started setup'

--- ChatApp/Controllers/ConversationsViewController.swift
+++ ChatApp/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  ChatApp
 //
 //  Created by Roy Park on 4/27/21.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
