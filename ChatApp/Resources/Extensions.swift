22fdd46affbe2ea9f10d48015224f50dc4011c17
Roy Park <roypark2638@gmail.com>
2021-04-28 00:40:06 -0700

    Added extensions

---
+++ ChatApp/Resources/Extensions.swift
@@ -0,0 +1,54 @@
+//
+//  Extensions.swift
+//  ChatApp
+//
+//  Created by Roy Park on 4/27/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return height + top
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return left + width
+    }
+}
+
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
