74b22ef8b8f7031a99288d757ad7cf9b5de34e61
Roy Park <roypark2638@gmail.com>
2021-04-28 00:41:16 -0700

    Implemented UI for login & signup VC

--- ChatApp/Controllers/LoginViewController.swift
+++ ChatApp/Controllers/LoginViewController.swift
@@ -8,22 +8,152 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "AppLogo")
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let emailField = AuthTextField(type: .email, title: nil)
+    
+    private let passwordField = AuthTextField(type: .password, title: nil)
+    
+    private let signInButton = AuthButton(type: .signIn, title: nil)
+    
+    private let signUpButton = AuthButton(type: .signUp, title: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Log In"
+        view.backgroundColor = .systemBackground
+        configureNavigationBar()
+        addSubviews()
+        addButtonActions()
+        emailField.delegate = self
+        passwordField.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        let imageSize = view.width/3
+        imageView.frame = CGRect(
+            x: (view.width-imageSize)/2,
+            y: view.safeAreaInsets.top,
+            width: imageSize,
+            height: imageSize
+        )
+        
+        emailField.frame = CGRect(
+            x: 20,
+            y: imageView.bottom + 10,
+            width: view.width - 40,
+            height: 50
+        )
+        
+        passwordField.frame = CGRect(
+            x: 20,
+            y: emailField.bottom + 16,
+            width: view.width-40,
+            height: 50
+        )
+        
+        signInButton.frame = CGRect(
+            x: 20,
+            y: passwordField.bottom + 46,
+            width: view.width-40,
+            height: 50
+        )
+        
+        signUpButton.frame = CGRect(
+            x: 20,
+            y: signInButton.bottom + 16,
+            width: view.width-40,
+            height: 50
+        )
+    }
+    
+    private func addSubviews() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(signInButton)
+        scrollView.addSubview(signUpButton)
     }
     
 
-    /*
-    // MARK: - Navigation
+    private func configureNavigationBar() {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        navigationController?.navigationBar.tintColor = .label
     }
-    */
 
+    private func addButtonActions() {
+        signInButton.addTarget(
+            self,
+            action: #selector(didTapSignIn),
+            for: .touchUpInside
+        )
+        
+        signUpButton.addTarget(
+            self,
+            action: #selector(didTapSignUp),
+            for: .touchUpInside
+        )
+    }
+    
+    private func showAlertUserLoginError() {
+        let alert = UIAlertController(
+            title: "Ooops",
+            message: "Please check your input again.",
+            preferredStyle: .alert)
+        alert.addAction(UIAlertAction(
+                            title: "Dismiss",
+                            style: .cancel,
+                            handler: nil))
+        present(alert, animated: true)
+    }
+
+    
+    @objc private func didTapSignIn() {
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        
+        guard let email = emailField.text,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              let password = passwordField.text,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty,
+              password.count >= 6
+        else {
+            showAlertUserLoginError()
+            return
+        }
+    }
+    
+    @objc private func didTapSignUp() {
+        let vc = RegisterViewController()
+        navigationController?.pushViewController(vc, animated: true)
+
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailField {
+            passwordField.becomeFirstResponder()
+        }
+        else if textField == passwordField {
+            didTapSignIn()
+        }
+        return true
+    }
 }
--- ChatApp/Controllers/RegisterViewController.swift
+++ ChatApp/Controllers/RegisterViewController.swift
@@ -9,21 +9,149 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "AppLogo")
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let emailField = AuthTextField(type: .email, title: nil)
+    
+    private let usernameField = AuthTextField(type: .email, title: "username")
+    
+    private let passwordField = AuthTextField(type: .password, title: nil)
+    
+    private let signUpButton = AuthButton(type: .signUp, title: nil)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Create Account"
+        view.backgroundColor = .systemBackground
+        configureNavigationBar()
+        addSubviews()
+        addButtonActions()
+        emailField.delegate = self
+        passwordField.delegate = self
+        usernameField.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        let imageSize = view.width/3
+        imageView.frame = CGRect(
+            x: (view.width-imageSize)/2,
+            y: view.safeAreaInsets.top,
+            width: imageSize,
+            height: imageSize
+        )
+        
+        usernameField.frame = CGRect(
+            x: 20,
+            y: imageView.bottom + 10,
+            width: view.width - 40,
+            height: 50
+        )
+        
+        emailField.frame = CGRect(
+            x: 20,
+            y: usernameField.bottom + 16,
+            width: view.width - 40,
+            height: 50
+        )
+        
+        passwordField.frame = CGRect(
+            x: 20,
+            y: emailField.bottom + 16,
+            width: view.width-40,
+            height: 50
+        )
+        
+        signUpButton.frame = CGRect(
+            x: 20,
+            y: passwordField.bottom + 46,
+            width: view.width-40,
+            height: 50
+        )
+        
 
-        // Do any additional setup after loading the view.
+    }
+    
+    private func addSubviews() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(usernameField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(signUpButton)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureNavigationBar() {
+        navigationController?.navigationBar.tintColor = .label
     }
-    */
 
+    private func addButtonActions() {
+        signUpButton.addTarget(
+            self,
+            action: #selector(didTapSignUp),
+            for: .touchUpInside
+        )
+    }
+    
+    private func showAlertUserSignUpError() {
+        let alert = UIAlertController(
+            title: "Ooops",
+            message: "Please check your input again.",
+            preferredStyle: .alert)
+        alert.addAction(UIAlertAction(
+                            title: "Dismiss",
+                            style: .cancel,
+                            handler: nil))
+        present(alert, animated: true)
+    }
+    
+    @objc private func didTapSignIn() {
+        guard let email = emailField.text,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              let username = usernameField.text,
+              !username.trimmingCharacters(in: .whitespaces).isEmpty,
+              username.count >= 2,
+              let password = passwordField.text,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty,
+              password.count >= 6
+        else {
+            showAlertUserSignUpError()
+            return
+        }
+    }
+    
+    @objc private func didTapSignUp() {
+        usernameField.resignFirstResponder()
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+    }
+}
+
+extension RegisterViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == usernameField {
+            emailField.becomeFirstResponder()
+        }
+        else if textField == emailField {
+            passwordField.becomeFirstResponder()
+        }
+        else if textField == passwordField {
+            didTapSignUp()
+        }
+        return true
+    }
 }
