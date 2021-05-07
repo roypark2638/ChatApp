b2c8adfd5c6886b81ad6ad7484e7e50de79aca7a
Roy Park <roypark2638@gmail.com>
2021-05-06 19:27:20 -0700

    Added COnversationsVC spinner and tableview data and delegate.

--- ChatApp/Controllers/ConversationsViewController.swift
+++ ChatApp/Controllers/ConversationsViewController.swift
@@ -7,22 +7,74 @@
 
 import UIKit
 import FirebaseAuth
+import JGProgressHUD
 
 class ConversationsViewController: UIViewController {
+    
+    private let spinner = JGProgressHUD(style: .dark)
+    
+    private let noConversationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "No Chats"
+        label.textAlignment = .center
+        label.textColor = .secondaryLabel
+        label.isHidden = true
+        return label
+    }()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.isHidden = true
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Chats"
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.tintColor = .label
+        addSubviews()
+        fetchConversations()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .compose,
+            target: self,
+            action: #selector(didTapCompose))
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         validateAuth()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
         
+        noConversationLabel.center = view.center
+    }
+    
+    // MARK: - Methods
+    
+    private func fetchConversations() {
+        tableView.isHidden = false
+    }
+    
+    private func updateUI() {
+        
+    }
+    
+    private func addSubviews() {
+        view.addSubview(tableView)
+        view.addSubview(noConversationLabel)
     }
     
     private func validateAuth() {
@@ -38,6 +90,41 @@ class ConversationsViewController: UIViewController {
         }
         
     }
+    
+    // MARK: - Objc
+    
+    @objc private func didTapCompose() {
+        let vc = NewConversationViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        present(nav, animated: true)
+    }
 
 }
 
+// MARK: - UITableViewDelegate, UITableViewDataSource
+
+extension ConversationsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "cell",
+            for: indexPath
+        )
+        
+        cell.backgroundColor = .systemRed
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = ChatViewController()
+        vc.title = "Some username"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
