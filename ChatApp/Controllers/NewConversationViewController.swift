59668f48c941733f2e94b37f416ed4f7a55099b4
Roy Park <roypark2638@gmail.com>
2021-05-06 19:28:10 -0700

    setup NewConversationVC

--- ChatApp/Controllers/NewConversationViewController.swift
+++ ChatApp/Controllers/NewConversationViewController.swift
@@ -6,24 +6,59 @@
 //
 
 import UIKit
+import JGProgressHUD
 
 class NewConversationViewController: UIViewController {
+    
+    private let spinner = JGProgressHUD()
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search for Users..."
+        return searchBar
+    }()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "cell")
+        tableView.isHidden = true
+        return tableView
+    }()
+    
+    private let noResultLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Nothing Found"
+        label.textColor = .green
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        label.isHidden = true
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        searchBar.delegate = self
+        navigationController?.navigationBar.topItem?.titleView = searchBar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Cancel",
+            style: .done,
+            target: self,
+            action: #selector(didTapCancel)
+        )
+        searchBar.becomeFirstResponder()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func didTapCancel() {
+        dismiss(animated: true, completion: nil)
     }
-    */
 
+
+}
+
+extension NewConversationViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
 }
